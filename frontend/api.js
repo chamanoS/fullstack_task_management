61bd2fad7dc0530c51b7105cd29499f5a55b1c52
Chamano Ramashia <chamano.sr@gmail.com>
2023-07-06 10:28:28 +0200

    fetch function done

--- frontend/api.js
+++ frontend/api.js
@@ -3,8 +3,33 @@
 // Fetch all tasks from the backend
 async function fetchTasks() {
     const response = await fetch("http://localhost:3000/tasks");
-    const tasks = await response.jason();
+    const tasks = await response.json();
+    const allBlogs = document.getElementById("blog-lists");
+
+    tasks.forEach(task => {
+        var blog = "";
+
+        blog += '<div class="post">';
+        blog += '<h2 class="post-title">;';
+        blog += task.title;
+        blog += "</h2>";
+        blog += '<p class="post-date">';
+        blog += task.date;
+        blog += "</p>";
+        blog +='<p class="post-content">';
+        blog += task.content;
+        blog += '</p>';
+        blog += '<div class="post-buttons">';
+        blog += '<button class="edit-post">Edit</button>';
+        blog += '<button class="delete-post">Delete</button>';
+        blog += "</div>";
+        blog += "<br>";
+        blog += '<a href="#" class="read-more">Read More</a>';
+        blog += "</div>";
+
+        allBlogs.append(blog);
     
+    });
     
   }
   
